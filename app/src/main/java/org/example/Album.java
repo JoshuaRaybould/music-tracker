b9334cacde958459b64851bd261b6cf8f58aa2c1
JoshuaRaybould <joshua.raybould@outlook.com>
2024-12-14 19:38:27 +0000

    Create album and artist db entities

--- app/src/main/java/org/example/Album.java
+++ app/src/main/java/org/example/Album.java
@@ -1,6 +1,30 @@
 package org.example;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "album")
 public class Album {
     
-    private String name;
+    @Id @Column(name = "album_id") private int id;
+
+    @Column(name = "album_name") private String name;
+
+    public Album() {}
+
+    public Album(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
--- app/src/main/java/org/example/Artist.java
+++ app/src/main/java/org/example/Artist.java
@@ -1,6 +1,29 @@
 package org.example;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "artists")
 public class Artist {
     
-    private String name;
+    @Id @Column(name = "artist_id") private int id;
+    
+    @Column(name = "artist_name") private String name;
+
+    public Artist(){}
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
 }
