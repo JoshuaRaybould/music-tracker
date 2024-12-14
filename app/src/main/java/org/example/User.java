b3c26ca8d2abe2a092cbc467260411eb820bcad4
JoshuaRaybould <joshua.raybould@outlook.com>
2024-12-14 19:38:40 +0000

    Create user db entity

---
+++ app/src/main/java/org/example/User.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id @Column(name = "user_id") private int id;
+
+    @Column(name = "user_name") private String name;
+
+    public User(){}
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
