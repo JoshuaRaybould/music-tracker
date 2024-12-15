4d503bcc2c6a9194ab329f25a30ef6a683a17796
JoshuaRaybould <joshua.raybould@outlook.com>
2024-12-15 00:41:38 +0000

    Update to test many to many relationship

--- app/src/test/java/org/example/HibernateTest.java
+++ app/src/test/java/org/example/HibernateTest.java
@@ -1,12 +1,21 @@
 package org.example;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 public class HibernateTest {
 
@@ -25,7 +34,7 @@ public class HibernateTest {
    }
 
    @Test
-   void doStuff() {
+   void testUserCreation() {
 
       User user = new User("Josh");
       Session session = sessionFactory.openSession();
@@ -33,5 +42,44 @@ public class HibernateTest {
 
       session.persist(user);
       session.getTransaction().commit();
+      session.close();
    }
+
+   @Test
+   void testManyToMany() {
+      User user = new User("2nd guy");
+      Artist artist = new Artist("Kendrick Lamar");
+      Duration totalDuration = Duration.ofDays(60).plus(Duration.ofMinutes(60));
+      UserArtist userArtist = new UserArtist(user, artist, totalDuration, LocalDate.of(2020, 1, 8),
+            LocalDate.of(2020, 1, 8));
+      user.addUserArtist(userArtist);
+      artist.addUserArtist(userArtist);
+      Session session = sessionFactory.openSession();
+      session.beginTransaction();
+
+      session.persist(user);
+      session.persist(artist);
+      session.persist(userArtist);
+      session.getTransaction().commit();
+
+      // Begin a transaction
+      Transaction transaction = session.beginTransaction();
+
+      // Retrieve the object using the primary key
+      User usr = (User) session.get(User.class, 1L);
+
+      // Commit the transaction
+      transaction.commit();
+
+      Set<UserArtist> userArtists = usr.getUserUserArtists();
+      List<UserArtist> userArts = new ArrayList<>(userArtists);
+      UserArtist userArt = userArts.get(0);
+      Artist theArtist = userArt.getArtist();
+
+      assertEquals("Kendrick Lamar", theArtist.getName());
+      assertEquals(totalDuration, userArt.getTimeListened());
+
+      session.close();
+   }
+
 }
